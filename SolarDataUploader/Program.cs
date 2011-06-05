bb5a60337e26bdb802a90e9eb825f6f2b4598a5c
Simon Walker <github@helpmebot.org.uk>
2011-06-05 19:16:44 +0100

    include stuff

--- SolarDataUploader/Program.cs
+++ SolarDataUploader/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
+using Utility.Interaction.ExceptionHandler;
 
 namespace SolarDataUploader
 {
@@ -13,9 +14,22 @@ namespace SolarDataUploader
         [STAThread]
         static void Main()
         {
+
+            ExceptionHandlerConfiguration ehc = new ExceptionHandlerConfiguration();
+            ehc.gitHubRepository = "stwalkerster/solar-uploader";
+            ehc.contactEmailAddress = "solar@helpmebot.org.uk";
+            ehc.showTechnicalInformation = true;
+            ehc.bugTracker = "https://github.com/stwalkerster/solar-uploader/issues";
+            ExceptionHandler.setConfiguration(ehc);
+
+            Application.ThreadException += ExceptionHandler.threadExceptionEventHandler;
+            AppDomain.CurrentDomain.UnhandledException += ExceptionHandler.currentDomainUnhandledExceptionHandler;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
         }
+
     }
 }
